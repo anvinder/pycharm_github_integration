767b25e5741d8e309b9c1eaaa260b1fb14662cb5
anvinder_singh26@yahoo.co.in <anvinder_singh26@yahoo.co.in>
2021-07-25 18:02:03 -0700

    to learn and see how pycharm is integrated with github

--- test_file_pycharm_github_integration.py
+++ test_file_pycharm_github_integration.py
@@ -4,4 +4,10 @@
 # This line is added in github and then a pull/ fetch will be done on local machine to check if the updated file can be
 # fetched in pycharm. The fetch command in pycharm did not work but the pull command is able to merge the changes.
 # This line is added in pycharm now to see if commit + push is sufficient to commit the changes to github repo.
+# Doing direct push doesn't work but in pycharm if user does commit followed by push then it works.
+
+# If there is a local repo but there is no repo on git then the local changes cannot be pushed to github.
+# User needs to create a repo on github and then commit changes from local machine.
+
+
 
