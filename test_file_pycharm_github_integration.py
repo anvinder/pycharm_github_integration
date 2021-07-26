30fb419d8963f883e980494415df99b8aaf3786e
anvinder_singh26@yahoo.co.in <anvinder_singh26@yahoo.co.in>
2021-07-25 17:45:32 -0700

    to learn and see how pycharm is integrated with github

--- test_file_pycharm_github_integration.py
+++ test_file_pycharm_github_integration.py
@@ -1,1 +1,3 @@
 # This is a sample file just to learn and see how pycharm is integrated with github
+# Create a pycharm new project and then from the main menu, select VCS-> create github repository.
+# Create a new .py file and
