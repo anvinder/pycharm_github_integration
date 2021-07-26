8d7197d72f7a411629fb9426012c03cbec7a98d9
anvinder <anvinder_singh26@yahoo.co.in>
2021-07-25 18:13:17 -0700

    Update test_file_pycharm_github_integration.py

--- test_file_pycharm_github_integration.py
+++ test_file_pycharm_github_integration.py
@@ -9,4 +9,4 @@
 # If there is a local repo but there is no repo on git then the local changes cannot be pushed to github.
 # User needs to create a repo on github and then commit changes from local machine.
 
-#
+# Editing only on github to see diff in local copy and the one in the repo
